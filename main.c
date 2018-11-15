6507a3d117561180a16144ecbcb314dae87385a2
tjrwldnjs <sue06024@naver.com>
2018-11-15 16:08:37 +0900

    pointer4

--- main.c
+++ main.c
@@ -2,18 +2,57 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// int(*pf)(int, int); 는 함수를 가리키는 포인터 (함수포인터) 
 
-char *proverb="All that glister is not gold.";
-void setPointer(char **q)
+int addfunc(int a, int b)
 {
-	*q = proverb;
+	return a+b;
+}
+
+int subfunc(int a, int b)
+{
+	return a-b;
+}
+
+int mulfunc(int a, int b)
+{ 
+	return a*b;
+}
+
+int divfunc(int a, int b)
+{
+	return a/b;
 }
 
 int main(int argc, char *argv[]) {
 	
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%s\n",p);
+	int num1,num2;
+	char op;
+	int(*calfunc)(int, int);
+	
+	printf("input calculation : ");
+	scanf("%d %c %d", &num1, &op, &num2);
+	
+	switch(op)
+	{
+		case '+' :
+			calfunc = addfunc;	
+			break;
+			
+		case '-' :
+			calfunc = subfunc;
+			break;
+			
+		case '*' :
+			calfunc = mulfunc;
+			break;
+			
+		case '/' :
+			calfunc = divfunc;
+			break;		
+	}
+	
+	printf("result : %i\n", calfunc(num1,num2));
 	
 	return 0;
 } 
