6fd03e842ada7be8f6e4508ef80929532bbb124d
tjrwldnjs <sue06024@naver.com>
2018-11-15 16:33:11 +0900

    pointer5

--- main.c
+++ main.c
@@ -2,57 +2,51 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-// int(*pf)(int, int); 는 함수를 가리키는 포인터 (함수포인터) 
 
-int addfunc(int a, int b)
+void print_image(int image[5][5])
 {
-	return a+b;
+	int i,j;
+	int *ptr = &image[0][0];
+	
+	for(i=0; i<5; i++)
+	{
+		for(j=0; j<5; j++)
+		{
+			printf("%d ", *ptr);
+			ptr = ptr +1;
+		}
+		printf("\n");
+	}
 }
 
-int subfunc(int a, int b)
+void brighten_image(int image[5][5])
 {
-	return a-b;
+	int i,j;
+	int *ptr = &image[0][0];
+	
+	for(i=0; i<5; i++)
+	{
+		for(j=0; j<5; j++)
+		{
+			*ptr = *ptr+10;
+			ptr = ptr +1;
+		}
+		
+	}printf("\n");
 }
-
-int mulfunc(int a, int b)
-{ 
-	return a*b;
-}
-
-int divfunc(int a, int b)
-{
-	return a/b;
-}
-
 int main(int argc, char *argv[]) {
 	
-	int num1,num2;
-	char op;
-	int(*calfunc)(int, int);
+	int image[5][5] ={
+	{10, 20, 30, 40, 50}, 
+	{10, 20, 30, 40, 50}, 
+	{10, 20, 30, 40, 50}, 
+	{10, 20, 30, 40, 50}, 
+	{10, 20, 30, 40, 50} 	
+	};
 	
-	printf("input calculation : ");
-	scanf("%d %c %d", &num1, &op, &num2);
-	
-	switch(op)
-	{
-		case '+' :
-			calfunc = addfunc;	
-			break;
-			
-		case '-' :
-			calfunc = subfunc;
-			break;
-			
-		case '*' :
-			calfunc = mulfunc;
-			break;
-			
-		case '/' :
-			calfunc = divfunc;
-			break;		
-	}
-	
-	printf("result : %i\n", calfunc(num1,num2));
+	print_image(image); 
+	brighten_image(image); 
+	print_image(image); 
 	
 	return 0;
 } 
